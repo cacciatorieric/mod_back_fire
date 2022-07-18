2be19e9c4bdd81aa1f28c9353ba44a54cd19ad9a
Eric Cacciatori Bellini <eric.cacciatori@hotmail.com>
2022-07-17 21:07:27 -0300

    back 1 terminado

--- mod_back_end/lib/models/order_list.dart
+++ mod_back_end/lib/models/order_list.dart
@@ -1,9 +1,11 @@
+import 'dart:convert';
 import 'dart:math';
-
 import 'package:flutter/cupertino.dart';
-
+import 'package:mod_back_end/models/cart_item.dart';
+import 'package:mod_back_end/utils/constants.dart';
 import 'cart.dart';
 import 'order.dart';
+import 'package:http/http.dart' as http;
 
 class OrderList with ChangeNotifier {
   final List<Order> _items = [];
@@ -16,14 +18,73 @@ class OrderList with ChangeNotifier {
     return _items.length;
   }
 
+  Future<void> loadOrders() async {
+    _items.clear();
+    final resposta = await http.get(
+      Uri.parse('${Constants.ORDER_BASE_URL}.json'),
+    );
+    if (resposta.body == 'null') return;
+    Map<String, dynamic> data = jsonDecode(resposta.body);
+    data.forEach(
+      (orderId, orderData) {
+        _items.add(
+          Order(
+            id: orderId,
+            date: DateTime.parse(
+              orderData['date'],
+            ),
+            total: orderData['total'],
+            products: (orderData['products'] as List<dynamic>).map((item) {
+              return CartItem(
+                id: item['id'],
+                productId: item['productId'],
+                name: item['name'],
+                quantity: item['quantity'],
+                price: item['price'],
+              );
+            }).toList(),
+          ),
+        );
+      },
+    );
+    notifyListeners();
+    print(data);
+  }
+
   Future<void> addOrder(Cart cart) async {
+    final date = DateTime.now();
+    final future = await http.post(
+      Uri.parse(
+        '${Constants.ORDER_BASE_URL}.json',
+      ), //Uri representa uma string que faz alguma coisa, assim como a URL, sempre precisa ser .json
+      body: jsonEncode(
+        //O objeto que passaremos precisará passar por uma conversão, o jsonEncode faz essa conversão
+        //
+        {
+          'total': cart.totalAmount,
+          'date': date.toIso8601String(),
+          'products': cart.itens.values
+              .map(
+                (cartItem) => {
+                  'id': cartItem.id,
+                  'productId': cartItem.productId,
+                  'name': cartItem.name,
+                  'quantity': cartItem.quantity,
+                  'price': cartItem.price,
+                },
+              )
+              .toList(),
+        },
+      ),
+    );
+    final id = jsonDecode(future.body)['name'];
     _items.insert(
       0,
       Order(
-        id: Random().nextDouble().toString(),
+        id: id,
         total: cart.totalAmount,
         products: cart.itens.values.toList(),
-        date: DateTime.now(),
+        date: date,
       ),
     );
     notifyListeners();
--- mod_back_end/lib/pages/orders_page.dart
+++ mod_back_end/lib/pages/orders_page.dart
@@ -4,8 +4,25 @@ import 'package:mod_back_end/components/order.dart';
 import 'package:mod_back_end/models/order_list.dart';
 import 'package:provider/provider.dart';
 
-class OrdersPage extends StatelessWidget {
+class OrdersPage extends StatefulWidget {
   const OrdersPage({Key? key}) : super(key: key);
+
+  @override
+  State<OrdersPage> createState() => _OrdersPageState();
+}
+
+class _OrdersPageState extends State<OrdersPage> {
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    Provider.of<OrderList>(context, listen: false).loadOrders().then(
+      (_) {
+        setState(() => _isLoading = false);
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,10 +32,14 @@ class OrdersPage extends StatelessWidget {
         title: const Text('Meus Pedidos'),
       ),
       drawer: const AppDrawer(),
-      body: ListView.builder(
-        itemCount: orders.itemsCount,
-        itemBuilder: (ctx, i) => OrderWidget(order: orders.items[i]),
-      ),
+      body: _isLoading
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : ListView.builder(
+              itemCount: orders.itemsCount,
+              itemBuilder: (ctx, i) => OrderWidget(order: orders.items[i]),
+            ),
     );
   }
 }
